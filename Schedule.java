ae8938637d0e65cca0c042b2d3f3582920158a88
Sargent <sargentw@vdi-sccubu-021.lancs.local>
2022-05-23 09:09:06 +0100

    Updates on the Schedule form

--- Schedule.java
+++ Schedule.java
@@ -15,6 +15,28 @@ public class Schedule
     private JLabel priority;
     private JLabel time;
     private JButton conf;
+    private JComboBox hours;
+    private JComboBox mins;
+
+    private String hoursArray[]
+        ={"0","1", "2", "3", "4", "5",
+        "6", "7", "8", "9", "10",
+        "11","12","13","14", "15",
+        "16","17","18","19","20",
+        "21","22","23","24" };
+
+    private String minsArray[]
+        ={"0","1", "2", "3", "4", "5",
+        "6", "7", "8", "9", "10",
+        "11", "12", "13", "14", "15",
+        "16", "17", "18", "19", "20",
+        "21", "22", "23", "24", "25",
+        "26", "27", "28", "29", "30",
+        "31","32","33","34","35","36",
+        "37","38","39","40","41","42",
+        "43","44","45","46","47","48",
+        "49","50","51","52","53","54",
+        "55","56","57","58","59",};
 
     
     public Schedule(){
@@ -43,13 +65,23 @@ public class Schedule
         priority.setBounds(100, 150, 100, 20);
         container.add(priority);
 
-        time = new JLabel("Length:");
-        time.setBounds(100, 200, 100, 20);
+        time = new JLabel("Length: (hrs/mins)");
+        time.setBounds(100, 200, 150, 20);
         container.add(time);
+
+        hours = new JComboBox(hoursArray);
+        hours.setBounds(250, 200, 60, 20);
+        container.add(hours);
+
+        mins = new JComboBox(minsArray);
+        mins.setBounds(310, 200, 80, 20);
+        container.add(mins);
 
         conf = new JButton("Confirm");
         conf.setBounds(150, 450, 100, 30);
         container.add(conf);
+
+
 
         setVisible(true);
 
